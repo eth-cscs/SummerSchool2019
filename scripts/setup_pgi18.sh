c00a0e79db0c743c507e3b5f9c6c3c7a93751c73
Vasileios Karakasis <karakasis@cscs.ch>
2019-07-14 23:38:35 +0200

    Remove unnecessary setup file for PGI 18

--- scripts/setup_pgi18.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PGIV=18.4
-module swap PrgEnv-cray PrgEnv-pgi
-module use /apps/common/UES/pgi/$PGIV/modulefiles
-module rm pgi/17.5.0
-module load pgi/$PGIV
-export PGI_VERS_STR=$PGIV.0
